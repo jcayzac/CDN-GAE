e12f6ba1d7b8a3bb3c879a827282c9ce79a0a671
Julien Cayzac <julien.cayzac@gmail.com>
2011-09-27 19:49:14 +0900

    Replaced minify recipe with a script that produces combined files

--- README.txt
+++ README.txt
@@ -5,9 +5,8 @@ Steps to get it up and running:
 
 1/ Modify app/app.yaml with the name of your appengine application
 2/ Fill app/files with whatever you want to serve.
-3/ (optional) Install scripts/re-minify as a git pre-commit hook.
-4/ python2.5 bootstrap -d
-5/ bin/buildout
-6/ script/re-minify
-7/ bin/appcfg update app
+3/ python2.5 bootstrap -d
+4/ bin/buildout
+5/ bin/minify
+6/ bin/appcfg update app
 
--- app/files/crotte.js
+++ app/files/crotte.js
@@ -1,1 +1,177 @@
-(function(){var d=window,b=document;var c="http://1.cdn-jcayzac.appspot.com/files/";var i=function(n,m){var p,o=b.querySelector("script");p=b.createElement("script");p.async=true;p.src=n;p.onload=p.onreadystatechange=function(){if(!p.readyState||(/loaded|complete/).test(p.readyState)){p.onload=p.onreadystatechange=null;if(typeof m==="function"){m()}}};o.parentNode.insertBefore(p,o)};var e=function(o,n){if(b.getElementById(n)){return}var m=null;if(b.createStyleSheet){if(b.createStyleSheet(o)){m=b.createElement("style")}if(!m){return}}else{m=b.createElement("link");if(!m){return}m.rel="stylesheet";m.type="text/css";m.href=o}m.id=n;b.documentElement.firstChild.appendChild(m)};var k=function(n,m){e(c+n,m)};var h=function(q,p){var o=b.createElement("iframe");var n={src:q,frameborder:"0",allowfullscreen:"allowfullscreen"};for(var m in n){o.setAttribute(m,n[m])}o.className=p;return o};var g=function(o){var m=b.createElement("div");m.className="keep-aspect-ratio";var n=b.createElement("img");n.setAttribute("src",c+"16x9.png");m.appendChild(n);m.appendChild(o);return m};var a=function(m,n){return g(h(m,n))};var j=function(q,p){var m=b.querySelectorAll(q);for(var n=0;n<m.length;n++){var r=m[n],o=r.parentNode;o.insertBefore(p(r),r);o.removeChild(r)}};body=b.querySelector("body");if(body){if(!(body.className||"").match(/\bjs\b/)){var f=(body.className||"").split(/\s+/);f.push("js");body.className=f.join(" ")}}k("fonts.css?gz","fontscss");k("gist.css?gz","gistcss");k("keep-aspect-ratio.css.css?gz","karcss");var l=false;(function(o){if(b.readyState==="complete"){setTimeout(o,1)}else{if(b.addEventListener){var m;m=function(){b.removeEventListener("DOMContentLoaded",m,false);o()};b.addEventListener("DOMContentLoaded",m,false);d.addEventListener("load",o,false)}else{if(b.attachEvent){var m;m=function(){if(b.readyState==="complete"){b.detachEvent("onreadystatechange",m);o()}};b.attachEvent("onreadystatechange",m);d.attachEvent("onload",o);var n=false;try{n=d.frameElement==null}catch(p){}if(b.documentElement.doScroll&&n){var q;q=function(){if(l){return}try{b.documentElement.doScroll("left")}catch(r){setTimeout(q,1);return}o()};q()}}}}})(function(){l=true;j("youtube",function(r){return a("//www.youtube.com/embed/"+r.getAttribute("video")+"?hd=1&autohide=1&fs=1&iv_load_policy=3&loop=1&rel=0&showsearch=0& showinfo=0&modestbranding=1&enablejsapi=1","youtube")});j("vimeo",function(r){return a("//player.vimeo.com/video/"+r.getAttribute("video")+"?title=0&byline=0&portrait=0&loop=1","vimeo")});j("video",function(r){return g(r)});var o=b.querySelectorAll("gist");for(var n=0;n<o.length;n++){var q=o[n];var p=q.getAttribute("code");var m="on_gist_"+p+"_"+Math.floor(Math.random()*99999999+1);window[m]=(function(t,s,r){return function(u){delete window[r];var A=b.createElement("div");A.innerHTML=u.div;var z=A.querySelectorAll(".gist-data");for(var w=0;w<z.length;w++){var v=u.files[w];if(v.substr(0,8)=="gistfile"){continue}var y=b.createElement("a");y.href="https://raw.github.com/gist/"+s+"/"+v;y.innerHTML=v;y.target="_blank";y.setAttribute("data-repo",s);y.setAttribute("data-filename",v);y.className="gist-raw-link font-sans";z[w].appendChild(y)}t.parentNode.insertBefore(A.firstChild,t);t.parentNode.removeChild(t)}})(q,p,m);i("https://gist.github.com/"+p+".json?callback="+m)}})})();
+(function() {
+	var W=window, D=document;
+	var base = 'http://1.cdn-jcayzac.appspot.com/files/';
+
+	var load_script = function(s, cb) {
+		var js, fjs = D.querySelector('script');
+		js = D.createElement('script');
+		js.async=true;
+		js.src = s;
+		js.onload = js.onreadystatechange = function() {
+			if (!js.readyState || ( /loaded|complete/ ).test(js.readyState)) {
+				js.onload = js.onreadystatechange = null;
+				if (typeof cb === "function") { cb(); }
+			}
+		};
+		fjs.parentNode.insertBefore(js, fjs);
+	};
+
+	var load_stylesheet = function(src,i) {
+		if (D.getElementById(i)) {return}
+		var css=null;
+		if (D.createStyleSheet) {
+			if (D.createStyleSheet(src)) {
+				css = D.createElement('style');
+			}
+			if (!css) {return}
+		}
+		else {
+			css = D.createElement("link");
+			if (!css) {return}
+			css.rel  = "stylesheet"
+			css.type = "text/css"
+			css.href = src
+		}
+		css.id = i
+		D.documentElement.firstChild.appendChild(css);
+	};
+
+	var load_local_stylesheet = function(src,i) {
+		load_stylesheet(base+src,i);
+	};
+
+	var iframe = function(src, classname) {
+		var e = D.createElement("iframe");
+		var a = {
+		  'src':  src,
+		  'frameborder': '0',
+		  'allowfullscreen': 'allowfullscreen'
+		};
+		for(var x in a) {
+			e.setAttribute(x, a[x]);
+		}
+		e.className = classname;
+		return e;
+	};
+	
+	var wrap_16x9 = function(e) {
+		var x = D.createElement('div');
+		x.className = 'keep-aspect-ratio';
+		var img = D.createElement('img');
+		img.setAttribute('src', base+'16x9.png');
+		x.appendChild(img);
+		x.appendChild(e);
+		return x;
+	};
+
+	var iframe_16x9 = function(s, c) {
+		return wrap_16x9(iframe(s, c));
+	};
+
+	var subst=function(sel, fn) {
+		var elems = D.querySelectorAll(sel);
+		for (var i=0; i<elems.length; i++) {
+			var e = elems[i], parent = e.parentNode;
+			parent.insertBefore(fn(e), e);
+			parent.removeChild(e);
+		};
+	};
+
+	body=D.querySelector('body')
+	if (body) {
+		if (!(body.className || '').match(/\bjs\b/)) {
+			var classes = (body.className || '').split(/\s+/)
+			classes.push('js')
+			body.className = classes.join(' ')
+		}
+	}
+	load_local_stylesheet('combined.min.css?gz', 'crottecss');
+
+	var domIsReady=false;
+	(function(fn) {
+		if (D.readyState === "complete") {
+			setTimeout(fn, 1);
+		}
+		else if (D.addEventListener) {
+			var x;
+			x=function() {
+				D.removeEventListener("DOMContentLoaded", x, false);
+				fn();
+			};
+			D.addEventListener("DOMContentLoaded", x, false);
+			W.addEventListener("load", fn, false);
+		}
+		else if (D.attachEvent) {
+			var x;
+			x=function() {
+				if (D.readyState === "complete" ) {
+					D.detachEvent("onreadystatechange", x);
+					fn();
+				}
+			};
+			D.attachEvent("onreadystatechange", x);
+			W.attachEvent("onload", fn);
+			var toplevel = false;
+			try {
+				toplevel = W.frameElement == null;
+			} catch(e) {}
+			if ( D.documentElement.doScroll && toplevel ) {
+				var sc;
+				sc=function() {
+					if (domIsReady) {
+						return;
+					}
+					try {
+						D.documentElement.doScroll("left");
+					} catch(e) {
+						setTimeout( sc, 1 );
+						return;
+					}
+					fn();
+				}
+				sc();
+			}
+		}
+	})(function() {
+		domIsReady=true;
+
+		// videos
+		subst('youtube', function(e) {
+			return iframe_16x9('//www.youtube.com/embed/' + e.getAttribute('video') + '?hd=1&autohide=1&fs=1&iv_load_policy=3&loop=1&rel=0&showsearch=0& showinfo=0&modestbranding=1&enablejsapi=1', 'youtube');
+		});
+		subst('vimeo', function(e) {
+			return iframe_16x9('//player.vimeo.com/video/' + e.getAttribute('video') + '?title=0&byline=0&portrait=0&loop=1', 'vimeo');
+		});
+		subst('video', function(e) {
+			return wrap_16x9(e);
+		});
+		// code
+		var gists = D.querySelectorAll('gist');
+		for (var gi=0; gi<gists.length; gi++) {
+			var e = gists[gi];
+			var code = e.getAttribute('code');
+			var cb = 'on_gist_' + code + '_' + Math.floor(Math.random()*99999999+1);
+			window[cb] = (function(e, code, cb){ return function(x) {
+				delete window[cb];
+				var div = D.createElement('div');
+				div.innerHTML = x.div;
+				var pre = div.querySelectorAll('.gist-data');
+				for (var i=0; i<pre.length; i++) {
+					var filename = x.files[i];
+					if (filename.substr(0, 8) == 'gistfile') {continue};
+					var link = D.createElement('a');
+					link.href = 'https://raw.github.com/gist/'+code+'/'+filename;
+					link.innerHTML = filename;
+					link.target = '_blank';
+					link.setAttribute('data-repo', code);
+					link.setAttribute('data-filename', filename);
+					link.className = 'gist-raw-link font-sans';
+					pre[i].appendChild(link);
+				};
+				e.parentNode.insertBefore(div.firstChild, e);
+				e.parentNode.removeChild(e);
+			}; })(e, code, cb);
+			load_script('https://gist.github.com/' + code + '.json?callback='+cb);
+		};
+	})
+})();
